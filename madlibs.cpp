680e835201c6d2a55a5f3fa1ea5e9a9c4a1642bd
gavind20 <gjdacumos@mail.csuchico.edu>
2018-09-12 21:45:28 -0700

    Adds printed words + adlib

--- madlibs.cpp
+++ madlibs.cpp
@@ -5,47 +5,36 @@ using namespace std;
 
 int main()
 {
-    //Variables
 
-    char place, p;
-    char adj, a;
-    char adj2;
-    char adj3;
-    char noun, n;
-    int integer, i;
-    char pluralnoun, l;
-    
-    //Inputs
-    char place;
+    //Variables and Inputs
+    string place;
     std::cout<<"Add a place:\n"<<std::endl;
     std::cin>>place;
-    char adj;
+    string adj;
     std::cout<<"Add an adjective ending with -er:\n"<<std::endl;
     std::cin>>adj;
-    char pluralnoun;
+    string pluralnoun;
     std::cout<<"Add a pl. noun:\n"<<std::endl;
     std::cin>>pluralnoun;
-    char integer;
+    int integer;
     std::cout<<"Add an integer:\n"<<std::endl;
     std::cin>>integer;
-    int adj2;
+    string adj2;
     std::cout<<"Add an adjective:\n"<<std::endl;
     std::cin>>adj2;
-    char adj3;
-    std::cout<<"Add an adjective:\n"<<std::endl;
+    string adj3;
+    std::cout<<"Add an adjective ending with -er:\n"<<std::endl;
     std::cin>>adj3;
-    char noun;
+    string noun;
     std::cout<<"Add a noun:\n"<<std::endl;
     std::cin>>noun;
 
     //Print
  
-    std::cout<<place<<std::endl;
-    std::cout<<"is"<<adj<<std::endl;
-    std::cout<<"now, since the"<<pluralnoun<<std::endl;
-    std::cout<<"conquered it. What were we thinking, trying to fight back against"<<integer<<std::endl;
-    std::cout<<"of those"<<adj2<<"things. Maybe things would be"<<adj3<<std::endl;
-    std::cout<<"if we were more like the"<<noun<<std::endl;
+    std::cout<<place<<" is "<<adj<<" now, since the "<<pluralnoun<<std::endl;
+    std::cout<<" conquered it. What were we thinking, trying to fight back against "<<integer<<std::endl;
+    std::cout<<" of those "<<adj2<<" things. Maybe things would be "<<adj3<<std::endl;
+    std::cout<<" if we were more like the "<<noun<<std::endl;
            
     return 0;
 }

b0318387a21234aaadda6e74e02df20ef1a92307
gavind20 <gjdacumos@mail.csuchico.edu>
2018-09-12 18:01:24 -0700

    Adds variables

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,39 @@
-//Authors: 
+//Authors: Gavin Dacumos
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
+    //Variables
 
-  return 0;
+    char place, p;
+    char adj, a;
+    char noun, n;
+    int integer, i;
+    char pluralnoun, l;
+    
+    //Inputs
+    {
+    std::cout<<"Add a place:\n"<<std::endl;
+    std::cin>>place;
+    }
+    {
+    std::cout<<"Add an adjective:\n"<<std::endl;
+    std::cin>>adj;
+    }
+    {
+    std::cout<<"Add a pl. noun:\n"<<std::endl;
+    std::cin>>pluralnoun;
+    }
+    {
+    std::cout<<"Add an integer:\n"<<std::endl;
+    std::cin>>integer;
+    }
+    //Printing
+
+
+  
+
+    return 0;
 }
